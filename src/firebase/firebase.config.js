a72bfc905b3f0c0064c28e2f562529b33ec39e57
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-10-28 11:42:20 +0600

    create home, main, route component and slick slider carousel

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCgzBUFi-SyLrs06k2Io4HIT7TXyZtNLtk",
+    authDomain: "react-firebase-9-fccb9.firebaseapp.com",
+    projectId: "react-firebase-9-fccb9",
+    storageBucket: "react-firebase-9-fccb9.appspot.com",
+    messagingSenderId: "366131642420",
+    appId: "1:366131642420:web:14ebe7ed49b4c6e5e9ca61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
